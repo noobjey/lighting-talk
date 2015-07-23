d45389f32aadd84c35c2ada16a997ab731152b44
Jason Wright <noobjey@gmail.com>
2015-07-22 18:53:08 -0600

    fizz buzz done

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,13 @@
 function FizzBuzz(){
-  this.go = function() {
-    return 'fizz'
+  this.go = function(number) {
+    if (number % 5 === 0 && number % 3 == 0)
+      return 'fizzbuzz';
+    else if (number % 5 === 0)
+      return 'buzz';
+    else if (number % 3 === 0)
+      return 'fizz';
+    else
+      return number;
+    end;
   }
 }
